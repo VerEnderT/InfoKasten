df0454f7da257dcffac69a7eeec43314a77cacda
VerLinuxT <53666253+VerEnderT@users.noreply.github.com>
2022-10-31 07:31:49 +0100

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-command = ("lshw -C system").split(" ")
-print(command)
-complete = subprocess.run(command, capture_output=True)
-ergebnis = str(complete.stdout)  # .split("\\n")
-print(ergebnis)
-
